30e38f610411dfce06086e699c2857e95631d59e
Monadic Cat <greenmonadcat@gmail.com>
2019-09-10 13:29:35 -0500

    New output format

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@
 //! That's it.
 //! The output format is unstable, because
 //! [`mice`](https://crates.io/crates/mice) is unstable.
+use mice::prelude::*;
 use std::env::args;
-use mice::roll;
 
 fn main() {
+    let default_format = MiceFormat::new().verbose().total_right();
     let mut a = args();
     a.next();
     let input = a.fold(String::new(), |a, x| a + " " + &x);
     let result = roll(&input).unwrap();
-    println!("{}", result);
+    println!("{}", result.format(default_format))
 }
