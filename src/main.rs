f26f78ca78ea075356b6581cae2b6d3800633a54
Monadic Cat <greenmonadcat@gmail.com>
2020-05-13 14:55:15 -0500

    async refactor, rolls refactor, add conjunctions for roll reasons

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
-use mice::{util::roll_capped, FormatOptions as MiceFormat};
+use mice::{FormatOptions as MiceFormat};
 mod initiative;
 use initiative::pathfinder_initiative;
 use serenity::{
@@ -10,94 +10,138 @@ use serenity::{
     },
     model::{channel::Message, gateway::Ready},
     prelude::*,
+    async_trait,
 };
 
-fn reply(ctx: &mut Context, msg: &Message, r: &str) -> CommandResult {
+async fn reply(ctx: &Context, msg: &Message, r: &str) -> CommandResult {
     let instance_name = std::env::var("INSTANCE_MESSAGE_PREFIX").unwrap_or_default();
     msg.channel_id.say(
         &ctx.http,
         format!("{}{} {}", instance_name, msg.author.mention(), r),
-    )?;
+    ).await?;
     Ok(())
 }
 
 #[command]
-fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "pong")
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "pong").await
 }
 // Suggested by Lasersquid
 #[command]
-fn potatoes(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "tatoes")
+async fn potatoes(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "tatoes").await
 }
 #[command]
-fn happy(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "La la la la la la! Sing the happy song!")
+async fn happy(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "La la la la la la! Sing the happy song!").await
 }
 #[command]
-fn po(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "tatoes")
+async fn po(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "tatoes").await
 }
 #[command]
-fn literal(ctx: &mut Context, msg: &Message, arg: Args) -> CommandResult {
+async fn literal(ctx: &Context, msg: &Message, arg: Args) -> CommandResult {
     reply(
         ctx,
         msg,
         &format!("Literal {}", arg.message().trim()),
-    )
+    ).await
 }
 #[command]
-fn gargamel(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "I think you mean \"Gargalsmell\"!")
+async fn gargamel(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "I think you mean \"Gargalsmell\"!").await
 }
 
-fn roll_smart(exp: &str) -> Result<String, mice::util::UtilError> {
-    match roll_capped(exp, 10000) {
-        Ok(x) => {
-            let first = x.format(MiceFormat::new().total_right());
-            let second;
-            if first.len() < 1900 {
-                Ok(first)
-            } else if {
-                second = x.format(MiceFormat::new().concise().total_right());
-                second.len() < 1900
-            } {
-                Ok(second)
-            } else {
-                Ok(x.total().to_string())
-            }
-        }
-        Err(x) => Err(x),
+const MAX_REPLY_LENGTH: usize = 1900;
+
+fn format_smart(exp: mice::ExpressionResult) -> String {
+    let first = exp.format(MiceFormat::new().total_right());
+    let second;
+    if first.len() < MAX_REPLY_LENGTH {
+        first
+    } else if {
+        second = exp.format(MiceFormat::new().concise().total_right());
+        second.len() < MAX_REPLY_LENGTH
+    } {
+        second
+    } else {
+        exp.total().to_string()
     }
 }
 
-fn roll_with_reason(exp: &str) -> Result<String, mice::util::UtilError> {
-    match exp.find("to") {
-        Some(x) => Ok(format!("{} {}", roll_smart(&exp[..x])?.to_ascii_lowercase(), &exp[x..])),
-        None => roll_smart(exp),
+#[derive(Debug)]
+enum MaybeReasonedDice {
+    Reasoned(Vec<mice::unstable::parse::Expr>),
+    Unreasoned(Vec<mice::unstable::parse::Expr>),
+}
+
+fn reasoned_dice(input: &str) -> nom::IResult<&str, MaybeReasonedDice> {
+    use nom::{bytes::complete::tag, multi::many1, branch::alt};
+    use mice::unstable::parse;
+    let (i, dice) = parse::dice(input)?;
+    let a = many1(parse::whitespace)(i).and_then(|(i, _)| {
+        let _ = alt((tag("to"), tag("for"), tag("because"), tag("#")))(i)?;
+        Ok((i, ()))
+    });
+    match a {
+        Ok((i, _)) => Ok((i, MaybeReasonedDice::Reasoned(dice))),
+        Err(_) => Ok((i, MaybeReasonedDice::Unreasoned(dice))),
     }
 }
+
+const ROLL_CAP: i64 = 10000;
 
 #[command]
 #[aliases("r")]
-fn roll(ctx: &mut Context, msg: &Message, arg: Args) -> CommandResult {
-    match roll_with_reason(arg.message()) {
-        Ok(x) => reply(ctx, msg, &x),
-        Err(x) => reply(ctx, msg, &format!("{}", x)),
+async fn roll(ctx: &Context, msg: &Message, arg: Args) -> CommandResult {
+    use mice::unstable::util::roll_exp_capped;
+    match reasoned_dice(arg.message()) {
+        Ok((reason, MaybeReasonedDice::Reasoned(dice))) => {
+            if reason.len() > MAX_REPLY_LENGTH {
+                reply(ctx, msg, "reason given is too long.").await
+            } else {
+                let res = roll_exp_capped(dice, ROLL_CAP);
+                match res {
+                    Ok(x) => {
+                        let dice_msg = format_smart(x);
+                        let resp = format!("{} {}", dice_msg, reason);
+                        if resp.len() > MAX_REPLY_LENGTH {
+                            reply(ctx, msg, &dice_msg).await?;
+                            reply(ctx, msg, &reason).await
+                        } else {
+                            reply(ctx, msg, &resp).await
+                        }
+                    },
+                    Err(x) => reply(ctx, msg, &format!("{}", x)).await,
+                }
+            }
+        },
+        Ok((post_text, MaybeReasonedDice::Unreasoned(dice))) => {
+            if post_text.trim() != "" {
+                reply(ctx, msg, "you've specified an invalid dice expression").await
+            } else {
+                let res = roll_exp_capped(dice, ROLL_CAP);
+                match res {
+                    Ok(x) => reply(ctx, msg, &format!("{}", format_smart(x))).await,
+                    Err(x) => reply(ctx, msg, &format!("{}", x)).await,
+                }
+            }
+        },
+        Err(_) => { reply(ctx, msg, "you've specified an invalid dice expression").await },
     }
 }
 
 #[command]
-fn pinit(ctx: &mut Context, msg: &Message, arg: Args) -> CommandResult {
+async fn pinit(ctx: &Context, msg: &Message, arg: Args) -> CommandResult {
     match pathfinder_initiative(arg.message().trim()) {
-        Ok((_, x)) => reply(ctx, msg, &x),
-        Err(_) => reply(ctx, msg, "invalid initiative bonus listing."),
+        Ok((_, x)) => reply(ctx, msg, &x).await,
+        Err(_) => reply(ctx, msg, "invalid initiative bonus listing.").await,
     }
 }
 
 #[command]
-fn goodnight(ctx: &mut Context, msg: &Message) -> CommandResult {
-    reply(ctx, msg, "Sleep is for the weak, but goodnight.")
+async fn goodnight(ctx: &Context, msg: &Message) -> CommandResult {
+    reply(ctx, msg, "Sleep is for the weak, but goodnight.").await
 }
 
 #[group]
@@ -105,23 +149,28 @@ fn goodnight(ctx: &mut Context, msg: &Message) -> CommandResult {
 struct Green;
 
 struct Handler;
+#[async_trait]
 impl EventHandler for Handler {
-    fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
 
 const TOKEN_NAME: &str = "MBOT_TOKEN";
-fn main() {
+#[tokio::main]
+async fn main() {
     let token = std::env::var(TOKEN_NAME)
         .unwrap_or_else(|_| panic!("Expected evironment variable: {}", TOKEN_NAME));
-    let mut client = Client::new(&token, Handler).expect("Error starting client.");
-    client.with_framework(
-        StandardFramework::new()
-            .configure(|c| c.prefix("!"))
-            .group(&GREEN_GROUP),
-    );
-    if let Err(reason) = client.start() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GREEN_GROUP);
+    let client = Client::new(&token).event_handler(Handler);
+
+    let client = client.framework(framework);
+
+    let mut client = client.await.expect("Error starting client.");
+
+    if let Err(reason) = client.start().await {
         println!("Client error {:#?}", reason);
     }
 }
