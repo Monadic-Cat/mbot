8faaf62e95bce3f16ca77156c35430b9c2095064
Monadic Cat <greenmonadcat@gmail.com>
2020-08-19 09:22:00 -0500

    clean up and reduce to minimum parsing for this feature

--- src/internal_rolls.rs
+++ src/internal_rolls.rs
@@ -1,7 +1,6 @@
 //! Parsing roll commands from normal messages,
 //! 
-//! in the form of "...normal text...(roll: <dice expression>)...normal text..."
-use ::core::num::NonZeroU8;
+//! in the form of "...normal text...(roll: &lt;dice expression>)...normal text..."
 use ::mice::parse::{dice, whitespace, Expression, InvalidDie};
 use ::nom::{bytes::complete::tag, multi::many0, sequence::tuple, IResult};
 
@@ -14,14 +13,6 @@ fn internal_roll(input: &str) -> IResult<&str, Result<Expression, InvalidDie>> {
         tag(")"),
     ))(input)?;
     Ok((input, res))
-}
-
-enum ParseState {
-    RollingEnabled,
-    RollingDisabled {
-        backticks: NonZeroU8,
-        block_quote: bool,
-    },
 }
 
 #[derive(Debug)]
@@ -56,33 +47,30 @@ impl ParsedMessage {
 pub(crate) fn message(input: &str) -> ParsedMessage {
     // Time to scroll across the thing uwu
     let mut place = input;
-    let mut state = ParseState::RollingEnabled;
     let mut info = ParsedMessage::new();
     while place.len() > 1 {
-        match state {
-            ParseState::RollingEnabled => {
-                // Somewhere, we want to do our checks for moving to the RollingDisabled state.
-                match internal_roll(place) {
-                    // The current place is a valid internal roll command.
-                    // Store the parsed result of that, and scroll past.
-                    Ok((input, res)) => {
-                        info.rolls.push(res);
-                        place = input;
-                    }
-                    // The current place is not a syntactically valid internal roll command.
-                    // That does not make this message invalid.
-                    // Just move forward one step.
-                    // Note that this can panic by creating an invalid UTF-8 slice.
-                    // We should probably change the dice parsers,
-                    // which do not inherently require valid UTF-8 input.
-                    // That change would essentially be the replacement of
-                    // `&str` with `&[u8]` in a number of places.
-                    // It may also be useful to make an iterator
-                    // for the creation of shrinking slices across our input.
-                    Err(_) => place = &place[1..],
-                }
+        match internal_roll(place) {
+            // The current place is a valid internal roll command.
+            // Store the parsed result of that, and scroll past.
+            Ok((input, res)) => {
+                info.rolls.push(res);
+                place = input;
             }
-            ParseState::RollingDisabled { .. } => {}
+            // The current place is not a syntactically valid internal roll command.
+            // That does not make this message invalid.
+            // Just move forward one step.
+            // Note that this can panic by creating an invalid UTF-8 slice.
+            // We should probably change the dice parsers,
+            // which do not inherently require valid UTF-8 input.
+            // That change would essentially be the replacement of
+            // `&str` with `&[u8]` in a number of places.
+            // It may also be useful to make an iterator
+            // for the creation of shrinking slices across our input.
+            Err(::nom::Err::Failure((_, ::nom::error::ErrorKind::TooLarge))) => {
+                place = &place[1..];
+                info.rolls.push(Err(InvalidDie));
+            },
+            _ => place = &place[1..],
         }
     }
     info
