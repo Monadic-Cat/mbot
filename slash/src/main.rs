dd13fb9b04f83ed3ddbc970c52a243f37e865618
Monadic Cat <greenmonadcat@gmail.com>
2020-12-18 19:52:47 -0600

    guild slash command registration

--- slash/src/main.rs
+++ slash/src/main.rs
@@ -12,7 +12,56 @@ mod api {
     /// Discord API version against which this is written.
     // I'm not going to attempt to support older versions of the Discord API,
     // since nobody but Discord provides it.
-    pub(crate) const API_VERSION: u8 = 8;
+    pub(crate) const VERSION: u8 = 8;
+    pub(crate) mod endpoint {
+        pub(crate) fn guild_slash(application_id: u64, guild_id: u64) -> String {
+            format!(
+                "{}/v{}/applications/{}/guilds/{}/commands",
+                super::BASE,
+                super::VERSION,
+                application_id,
+                guild_id
+            )
+        }
+        pub(crate) fn global_slash(application_id: u64) -> String {
+            format!(
+                "{}/v{}/applications/{}/commands",
+                super::BASE,
+                super::VERSION,
+                application_id
+            )
+        }
+    }
+    /// Calling this with a Slash Command that uses an already used name
+    /// for your application will updated the existing command.
+    pub(crate) async fn upsert_slash(
+        client: &::reqwest::Client,
+        application_id: super::ApplicationId,
+        auth: &super::Auth,
+        slash: &super::Slash,
+    ) -> Result<String, ::reqwest::Error> {
+        use super::{GuildId, Slash, Auth};
+        match slash {
+            Slash::Guild {
+                schema,
+                guild_id: GuildId { id: guild_id },
+            } => {
+                client
+                    .post(&endpoint::guild_slash(application_id.id, *guild_id))
+                    .json(&schema)
+                    .header("Authorization", match auth {
+                        Auth::BotToken(token) => format!("Bot {}", token),
+                        Auth::BearerToken(token) => format!("Bearer {}", token),
+                    })
+                    .send()
+                    .await?
+                    .text()
+                    .await
+            }
+            // This is the same as above, but with a different endpoint and no guild_id.
+            Slash::Global { .. } => todo!("registering global slashes"),
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -64,7 +113,28 @@ struct SlashSchema {
 /// Discord Guild ID
 #[derive(Debug, StructOpt, Serialize)]
 #[serde(transparent)]
-struct GuildId { id: u64 }
+struct GuildId {
+    id: u64,
+}
+
+/// Discord Application ID
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
+#[serde(transparent)]
+struct ApplicationId {
+    id: u64,
+}
+
+#[derive(Debug, Deserialize)]
+enum Auth {
+    BotToken(String),
+    BearerToken(String),
+}
+
+#[derive(Debug, Deserialize)]
+struct AppConfig {
+    id: ApplicationId,
+    auth: Auth,
+}
 
 #[derive(Debug, StructOpt)]
 enum Kind {
@@ -74,26 +144,61 @@ enum Kind {
     Global,
 }
 
+enum Slash {
+    Guild {
+        schema: SlashSchema,
+        guild_id: GuildId,
+    },
+    Global {
+        schema: SlashSchema,
+    },
+}
+
 /// Doing stuff with slash commands.
 #[derive(Debug, StructOpt)]
 enum Opt {
+    /// Registers or updates a slash command.
     Register {
         /// Path to a command schema.
         path: PathBuf,
+        /// Path to app configuration.
+        app_path: PathBuf,
         #[structopt(flatten)]
         kind: Kind,
     },
 }
 
-fn main() {
+#[::tokio::main]
+async fn main() {
     let opt = Opt::from_args();
     match opt {
-        Opt::Register { path, kind } => {
-            let input = BufReader::new(File::open(path).expect("couldn't open schema file"));
+        Opt::Register { path, kind, app_path } => {
+            let command_input = BufReader::new(File::open(path).expect("couldn't open schema file"));
+            let app_input = BufReader::new(File::open(app_path).expect("couldn't open app file"));
             let schema: SlashSchema =
-                ::ron::de::from_reader(input).expect("couldn't deserialize command schema");
+                ::ron::de::from_reader(command_input).expect("couldn't deserialize command schema");
+            let app: AppConfig = ::ron::de::from_reader(app_input)
+                .expect("couldn't deserialize app configuration");
             println!("Schema: {:#?}", schema);
-            println!("JSON Schema: {}", serde_json::to_string_pretty(&schema).unwrap());
+            println!(
+                "JSON Schema: {}",
+                serde_json::to_string_pretty(&schema).unwrap()
+            );
+            let client = ::reqwest::Client::new();
+            match kind {
+                Kind::Guild(guild_id) => {
+                    let result = api::upsert_slash(&client, app.id, &app.auth, &Slash::Guild {
+                        guild_id, schema
+                    }).await;
+                    println!("Result: {:?}", result);
+                },
+                Kind::Global => {
+                    let result = api::upsert_slash(&client, app.id, &app.auth, &Slash::Global {
+                        schema
+                    }).await;
+                    println!("Result: {:?}", result);
+                },
+            }
         }
     }
 }
