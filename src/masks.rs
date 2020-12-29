f149b0728692d36015eeba1118a1ac700e653503
Monadic Cat <greenmonadcat@gmail.com>
2020-12-29 10:48:01 -0600

    allow move description without rolling dice; reject invalid input

--- src/masks.rs
+++ src/masks.rs
@@ -2,13 +2,11 @@
 
 use ::serenity::framework::standard::{macros::{command, group}, CommandResult, Args};
 use ::serenity::client::Context;
-use ::serenity::model::channel::Message;
+use ::serenity::model::{channel::Message, prelude::ChannelId};
 use crate::reply;
 
-async fn move_without_roll(ctx: &Context, msg: &Message,
-                           phrase: &str, desc: &str, capital: &str) -> CommandResult {
-    let id = msg.channel_id;
-    let author_line = format!("{} {}", msg.author.name, phrase);
+async fn move_without_roll_internal(ctx: &Context, id: ChannelId, author: &str, phrase: &str, desc: &str, capital: &str) -> CommandResult {
+    let author_line = format!("{} {}", author, phrase);
     let description = format!("**Description**\n{}", desc);
     id.send_message(&ctx.http, |m| m.embed(|e| {
         e.description(description).author(|a| a.name(author_line)).title(capital)
@@ -17,11 +15,29 @@ async fn move_without_roll(ctx: &Context, msg: &Message,
     Ok(())
 }
 
+async fn move_without_roll(ctx: &Context, msg: &Message,
+                           phrase: &str, desc: &str, capital: &str) -> CommandResult {
+    let id = msg.channel_id;
+    move_without_roll_internal(ctx, id, &msg.author.name, phrase, desc, capital).await
+}
+
 async fn move_with_roll(ctx: &Context, msg: &Message, mut args: Args,
                         phrase: &str, desc: &str, capital: &str) -> CommandResult {
     let id = msg.channel_id;
     let author_line = format!("{} {}", msg.author.name, phrase);
-    let label: i8 = args.single().unwrap_or(0);
+    let label: i8 = match args.current() {
+        Some("info") => {
+            return move_without_roll_internal(ctx, id, &msg.author.name, phrase, desc, capital).await;
+        },
+        Some(label) => match label.parse() {
+            Ok(x) => x,
+            Err(_) => {
+                return reply(ctx, msg, "invalid value for label").await;
+            },
+        },
+        None => 0,
+    };
+    args.advance();
     let (r1, r2) = {
         use ::rand::Rng;
         let mut rng = ::rand::thread_rng();
