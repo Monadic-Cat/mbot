6f017e84a622667460eacff7123b0537058be782
Monadic Cat <greenmonadcat@gmail.com>
2021-02-10 22:24:46 -0600

    change benchmark to use compat layer

--- mice/benches/my_benchmark.rs
+++ mice/benches/my_benchmark.rs
@@ -21,34 +21,16 @@ fn compare_formatting(c: &mut Criterion) {
             buf.clear();
         });
     });
-    let same_cfg_func = |mut f: ::mice::nfmt::simple::ExpressionFormatter| {
-        f.if_many(|f| {
-            f.terms(|mut f| {
-                f.for_kind(|f, kind| {
-                    use mice::nfmt::TermKind;
-                    match kind {
-                        TermKind::Dice => {
-                            f.text("(").expression().text(" → ")
-                                .partial_sums(mice::nfmt::PartialSumSignDirective::Plus)
-                                .text(")");
-                        },
-                        TermKind::Constant => {
-                            f.total();
-                        }
-                    }});
-            }).text(" = ");
-        }).total();
-    };
     group.bench_function("Simple New Backend", |b| {
         b.iter(|| {
-            let mut buf = String::with_capacity(2000);
-            mice::nfmt::simple::format_result(&dice_result, &mut buf, same_cfg_func);
+            mice::nfmt::simple::format_compat(&dice_result, format_cfg);
         });
     });
     let mut buf = String::with_capacity(2000);
     group.bench_function("Simple New Backend - With Reusing", |b| {
         b.iter(|| {
-            mice::nfmt::simple::format_result(&dice_result, &mut buf, same_cfg_func);
+            mice::nfmt::simple::format_compat_with(&dice_result, &mut buf, format_cfg);
+            black_box(&buf);
             buf.clear();
         });
     });
