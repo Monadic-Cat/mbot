68a09b1c88ecd8c5914a3b1f93a5eb96318e0d48
Vincent Robert Weis <vrobweis@gmail.com>
2021-06-21 18:33:46 -0500

    added test module to mice

---
+++ mice/src/tests.rs
@@ -0,0 +1,20 @@
+
+
+fn test_expr(dice_expr: &str) -> Result<String, String> {
+    let program = match crate::parse::parse_expression(dice_expr.as_bytes()) {
+        Ok((_input, (_tokens, proggy))) => proggy,
+        Err((_input, e)) => {
+            panic!("That's an invalid dice expression. {:?}", e);
+        },
+    };
+    let result = crate::interp::interpret(&mut ::rand::thread_rng(), &program);
+    match result {
+        Ok(output) => Ok(crate::interp::fmt::mbot_format_default(program.terms(), &output)),
+        Err(e) => return Err(format!("{:?}", e)),
+    }
+}
+
+#[test]
+fn test_basic_expressions() {
+    println!("{}", test_expr("1d20+1d6+8").unwrap());
+}
