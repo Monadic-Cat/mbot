40157f57535c28ad17ebc9948d6bc501d3b0a0cb
Monadic Cat <greenmonadcat@gmail.com>
2021-06-20 21:30:41 -0500

    update ast interpreter fuzz target

--- mice/fuzz/fuzz_targets/ast_interp.rs
+++ mice/fuzz/fuzz_targets/ast_interp.rs
@@ -6,9 +6,9 @@ use ::mice::interp::interpret;
 use ::rand_pcg::Pcg32;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok((_, program)) = parse_expression(data) {
+    if let Ok((_, (_, program))) = parse_expression(data) {
         use ::mice::cost::{cost, AstInterp, Price};
-        match cost::<AstInterp, _>(&program) {
+        match cost::<AstInterp, _>(&program, ()) {
             // Dice programs permitted because cost estimation
             // machinery thinks they're cheap enough.
             Price::Bounded(price) if price <= 200 => {
