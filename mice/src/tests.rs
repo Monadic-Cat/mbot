78494f502ba02d0aa1519131c0150c59ed934e28
Vincent Robert Weis <vrobweis@gmail.com>
2021-06-21 20:55:20 -0500

    added more tests for common cases

--- mice/src/tests.rs
+++ mice/src/tests.rs
@@ -4,7 +4,7 @@ fn test_expr(dice_expr: &str) -> Result<String, String> {
     let program = match crate::parse::parse_expression(dice_expr.as_bytes()) {
         Ok((_input, (_tokens, proggy))) => proggy,
         Err((_input, e)) => {
-            panic!("That's an invalid dice expression. {:?}", e);
+            return Err(format!("That's an invalid dice expression. {:?}", e));
         },
     };
     let result = crate::interp::interpret(&mut ::rand::thread_rng(), &program);
@@ -39,4 +39,25 @@ fn test_mixed_whitespace() {
 #[test]
 fn test_maturity() {
     println!("{}", test_expr("69d420   +    13d37").unwrap());
+}
+
+#[test]
+fn test_zero_sides() {
+    println!("{}", test_expr("d0").unwrap_err());
+}
+
+#[test]
+fn test_one_side() {
+    println!("{}", test_expr("d1").unwrap());
+}
+
+#[test]
+fn test_max_sides() {
+    println!("{}", test_expr("d9223372036854775807").unwrap());
+}
+
+
+#[test]
+fn test_keeping_highest_dice() {
+    println!("{}", test_expr("4d6k3").unwrap());
 }
